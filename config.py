55e0168609c0817897c3c8e900c4fdb20bb067c4
Dekris13 <dekrisgames@yandex.ru>
2024-07-02 10:28:00 +0300

    1_conf

---
+++ config.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+#from pydantic_settings import BaseSettings, SettingsConfigDict
+#from pydantic import SecretStr
+from sqlalchemy import create_engine
+
+# PostgreSQL
+engine = create_engine('postgresql:///postgres?host=localhost&port=5432&user=postgres&password=Lomik123!')
+
+
+token ='6727727488:AAEeirEyMPVTIayRFr0UKW-TtPyKwTsZd5Y'
+
+conn = 'postgresql:///postgres?host=localhost&port=5432&user=postgres&password=Lomik123!'
+
+file = 'D:\TelegaramBot\AsincBot.py'
+
+#class Settings(BaseSettings):
+   # bot_token:SecretStr
+   # model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+
+#config = Settings()
+
+
+
+
+
+
+
+
+
+
+
+
+
